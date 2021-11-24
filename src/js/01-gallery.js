12d869ad543ce3a7bd6f685e61216a46ac43177c
Yegorov Bogdan <eb.you93@gmail.com>
2021-11-24 22:16:56 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -29,15 +29,6 @@ gallery.on('show.simplelightbox', function () {
 	// Do something…
 });
 
-gallery.on('error.simplelightbox', function (e) {
-	console.log(e); // Some usefull information
-});
-
-// with jQuery nearly the same
-let gallery = $('.gallery a').simpleLightbox();
-gallery.on('show.simplelightbox', function () {
-	// Do something…
-});
 
 
 
