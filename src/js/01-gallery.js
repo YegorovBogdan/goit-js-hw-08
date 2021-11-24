92ee86e1cce010d749737a6bc52cfaae871b4d54
Yegorov Bogdan <eb.you93@gmail.com>
2021-11-24 20:57:30 +0200

    создает галлерею

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 // Change code below this line
 
-console.log(galleryItems);
+//gallery
+const galleryComponent = document.querySelector('.gallery');
+const addedGalleryCard = galleryCards(galleryItems);
+
+
+//список изображений
+function galleryCards(gallery){
+    return gallery
+    .map(({ original, preview, description }) => {
+        return `<div class="gallery__items">
+        <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+      </a>
+      </div>` ;
+    })
+    .join('');
+}
+
+galleryComponent.insertAdjacentHTML('beforeend', addedGalleryCard);
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function(){});
+
+
